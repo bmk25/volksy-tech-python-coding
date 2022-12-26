cb60efb27caec93c64b41ac9ebb5708193ef0a28
bmk25 <madhukiranb@brightcomgroup.com>
2022-12-26 10:56:35 +0000

    jfjdf

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i==99:
+for i in range(0, 100):
+    if i == 99:
         print("{}".format(i))
 
-    else: 
+    else:
 
         print("{:02}".format(i), end=", ")

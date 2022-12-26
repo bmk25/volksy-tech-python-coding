f3888b1ba038806f362a5a7cf2b3b8b27d9634a9
bmk25 <madhukiranb@brightcomgroup.com>
2022-12-26 08:34:49 +0000

    hello

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,10 +10,10 @@ elif number > 0:
         print("Last digit of", number, "is", n2, "and is greater than 5")
     else:
         print(
-               "Last digit of",
-	       number, 
-	       "is", 
-               n2,
-               "and is less than 6 and not 0")
+             "Last digit of",
+	     number, 
+	     "is", 
+             n2,
+             "and is less than 6 and not 0")
 else:
     print("Last digit of", number, "is", "0 and is 0")
